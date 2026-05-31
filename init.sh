8126e0ab58f3fe5aabcfa0a93652b68da84f68ca
Pero.Xie <perox@duck.com>
2026-05-31 18:58:21 +0800

    share history between terminal

--- init.sh
+++ init.sh
@@ -197,6 +197,13 @@ BashAlias
 _configureBashEnv(){
   cat << BashEnv > ~/.bashrc_custom_env
   export ASDF_DIR=~/.asdf
+# Avoid duplicates
+HISTCONTROL=ignoredups:erasedups
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+
+# After each command, append to the history file and reread it
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
 BashEnv
 }
 
