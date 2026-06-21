ef6ced57bf7ae73f4f833b063832018b4d90437b
Pero.Xie <perox@duck.com>
2026-06-21 18:19:33 +0800

    emacs: Add aider to emacs config

--- init.sh
+++ init.sh
@@ -301,6 +301,27 @@ configureEmacs(){
 
 (add-to-list 'auto-mode-alist '("\\\\.tsx\\\\'" . typescript-mode))
 
+(use-package aider
+  :ensure t
+  :config
+  ;; For latest claude sonnet model
+  (setq aider-args '("--model" "4o" "--no-auto-accept-architect")) ;; add --no-auto-commits if you don't want it
+  (setenv "OPENAI_API_KEY" "sk-no-key-needed")
+  (setenv "OPENAI_API_URL" "http://localhost:8080/v1")
+  ;; Or chatgpt model
+  ;; (setq aider-args '("--model" "o4-mini"))
+  ;; (setenv "OPENAI_API_KEY" <your-openai-api-key>)
+  ;; Or use your personal config file
+  ;; (setq aider-args `("--config" ,(expand-file-name "~/.aider.conf.yml")))
+  ;; ;;
+  ;; Optional: Set a key binding for the transient menu
+  (global-set-key (kbd "C-c a") 'aider-transient-menu) ;; for wider screen
+  ;; or use aider-transient-menu-2cols / aider-transient-menu-1col, for narrow screen
+  (aider-magit-setup-transients) ;; add aider magit function to magit menu
+  ;; auto revert buffer
+  (global-auto-revert-mode 1)
+  (auto-revert-mode 1))
+
 (use-package minuet
     :ensure t
     :bind
